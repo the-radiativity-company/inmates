f283301a21dd46753af75dc935bfed01596bdfe5
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-02-15 20:04:55 -0600

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='inmates',
+    version='0.0.1',
+    description='a CLI tool for listing inmates',
+    packages=find_packages(),
+    maintainer='Emmanuel I. Obi',
+    maintainer_email='withtwoemms@gmail.com',
+    url='',
+    include_package_data=True,
+    install_requires=[
+        'click==7.0'
+    ],
+    entry_points="""
+        [console_scripts]
+        inmates=inmates.cli:cli
+    """,
+)
