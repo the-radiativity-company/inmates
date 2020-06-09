8711208695fc331ef8d8b6070538084a293858cb
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-06-09 18:35:23 -0500

    updates config to reflect recent move

--- inmates/scraper/settings.py
+++ inmates/scraper/settings.py
@@ -9,10 +9,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'inmates_scraper'
+BOT_NAME = 'inmates.scraper'
 
-SPIDER_MODULES = ['inmates_scraper.spiders']
-NEWSPIDER_MODULE = 'inmates_scraper.spiders'
+SPIDER_MODULES = ['inmates.scraper.spiders']
+NEWSPIDER_MODULE = 'inmates.scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
