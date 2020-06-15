b51bbb455f0e82084c4c31678ab2810884396526
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-06-14 22:54:47 -0500

    updates fixture preparation to use curtailed list

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,6 +7,7 @@ from shlex import split
 from subprocess import CalledProcessError
 from subprocess import PIPE
 from subprocess import Popen as Proc
+from subprocess import run as runProc
 from typing import ByteString
 from typing import List
 from typing import Tuple
@@ -23,17 +24,21 @@ test_spiders = [Path(os.path.join(test_spiders_dir, file)) for file in os.listdi
     and not match(r'__.+__\.py', file)]
 
 
-def run(spiders: List[Path]) -> None:
+def prepare_fixtures(spiders: List[Path]) -> None:
     print('\nGenerating fixtures for:')
     # TODO (withtwoemms) -- use concurrent.futures to better handle multiple spiders
     for spider in spiders:
         print(f'....{spider}')
         spider_fixture = Path(f'{test_fixtures_dir}/{spider.stem}.json')
+        spider_fixture.write_text('')
 
         fixture_generation_command = split(f'scrapy runspider --set=ROBOTSTXT_OBEY=False {str(spider)} -o {str(spider_fixture)}')
         handle(*execute(Proc(fixture_generation_command, stderr=PIPE)))
-        formatted_fixture = handle(*execute(chain(Proc(split(f'cat {str(spider_fixture)}'), stdout=PIPE), split('jq .'))))
-        spider_fixture.write_text(formatted_fixture.decode('utf-8'))
+
+        contents_proc = Proc(split(f'cat {str(spider_fixture)}'), stdout=PIPE)
+        contents_dict = json.loads(handle(*execute(contents_proc)).decode('utf-8'))
+        ran = runProc(split('jq .'), stdout=PIPE, input=json.dumps(contents_dict[-5:]).encode())
+        spider_fixture.write_text(ran.stdout.decode('utf-8'))
 
 
 def execute(proc: Proc = None) -> Tuple[bytes, bytes]:
@@ -59,4 +64,4 @@ def chain(proc: Proc, *commands: List[str]):
         return proc
 
 
-run(test_spiders)
+prepare_fixtures(test_spiders)
