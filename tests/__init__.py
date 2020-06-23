ea1bf6a5f92afb0c45ca1065f7d3ffbb5cd18250
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-06-23 18:53:43 -0500

    messy transition

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,15 @@
 import json
 import os
 
+from inmates.utils import all_files_in
+
+from importlib import import_module
 from pathlib import Path
 from re import match
+from scrapy.crawler import Crawler
+from scrapy.http import Request
+from scrapy.http import Response
+from scrapy.settings import Settings
 from shlex import split
 from subprocess import CalledProcessError
 from subprocess import PIPE
@@ -49,6 +56,55 @@ def prepare_fixtures_for(spider_paths: List[Path], fixtures_dir: Path, fixture_t
         # STEP 3: write the formatted output to disk
         spider_fixture_path.write_text(formatted_json.stdout.decode('utf-8'))
 
+
+commissary = Path(__file__).absolute().parent.parent.joinpath('commissary')
+# all_roster_paths = set(Path(site).stem for site in all_files_in(commissary))
+all_roster_paths = dict([Path(site).stem, Path(site).absolute()] for site in all_files_in(commissary))
+all_spider_names = set(Path(spider).stem for spider in all_files_in('inmates/scraper/spiders'))
+
+
+spider_classes = {
+    spider_name: {
+        'class': getattr(import_module(f'inmates.scraper.spiders.{spider_name}'), f'{spider_name.title()}Roster'),
+        'url': all_roster_paths.get(spider_name)
+    } for spider_name in all_spider_names
+}
+
+all_spider_info = [
+    (
+        spider_name,
+        getattr(import_module(f'inmates.scraper.spiders.{spider_name}'), f'{spider_name.title()}Roster'),
+        all_roster_paths.get(spider_name)
+    ) for spider_name in all_spider_names
+]
+
+for spider_info in all_spider_info:
+    spider_name, spider_class, local_uri = spider_info
+    print(spider_name)
+
+from pprint import pprint
+pprint(spider_info)
+# for name, class_and_url in spider_classes.items():
+#     pprint(class_and_url['url'])
+#     pprint(class_and_url)
+#
+#     instance = class_and_url['class'](start_urls=[str(class_and_url['url'].absolute().as_uri())])
+#     print(instance)
+#     pprint(dir(instance))
+#     result = instance.start_requests()
+#     # print('RESULT: ', result)
+#     settings = Settings()
+#     settings.setmodule('inmates.scraper.settings', priority='project')
+#     # pprint(settings.attributes)
+#     crawler = Crawler(class_and_url['class'], settings=settings)
+#     engine = crawler._create_engine()
+#     r = engine.downloader.fetch(Request(url=class_and_url['url'].absolute().as_uri(), callback=instance.parse), instance)
+#     print(r)
+#     pprint(dir(r))
+#     print(r.result)
+#     print(type(r.result))
+#     pprint(dir(r.result))
+#     pprint(list(instance.parse(r.result)))
 
 test_spiders_dir = 'tests/scraper/spiders'
 test_fixtures_dir = Path('tests/fixtures')
