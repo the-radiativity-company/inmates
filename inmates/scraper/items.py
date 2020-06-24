5b09169598671ade1562cafbfe2e704cbc5a500d
pjsier <pjsier@gmail.com>
2020-06-24 09:08:27 -0500

    refactor: add attrs, move items validators

--- inmates/scraper/items.py
+++ inmates/scraper/items.py
@@ -4,27 +4,23 @@ from datetime import date
 import attr
 
 from .constants import RACE_VALUES
-
-
-def values_are_in_list(values_list):
-    """Create a validator based on an allowed list"""
-
-    def inner_value_is_in_list(instance, attribute, value):
-        if not all([v in values_list for v in value]):
-            raise ValueError(f"Provided value not in allowed list: {values_list}")
-
-    return inner_value_is_in_list
+from .validators import values_are_in_list
 
 
 @attr.s
 class InmateItem:
     id: str = attr.ib(default="")
     name: str = attr.ib(default="")
+    sex: typing.Optional[str] = attr.ib(default=None)
     race: typing.List[str] = attr.ib(
         validator=[values_are_in_list(RACE_VALUES)], factory=list
     )
+    birth_date: typing.Optional[date] = attr.ib(default=None)
     height: typing.Optional[int] = attr.ib(default=None)
     weight: typing.Optional[int] = attr.ib(default=None)
+    booking_date: typing.Optional[date] = attr.ib(default=None)
     start_date: typing.Optional[date] = attr.ib(default=None)
     end_date: typing.Optional[date] = attr.ib(default=None)
+    charges: typing.List[str] = attr.ib(factory=list)
+    bail_amount: typing.Optional[int] = attr.ib(default=None)
     extra: typing.Dict = attr.ib(factory=dict)
