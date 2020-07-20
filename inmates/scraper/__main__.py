0a40758b69ef5d27a0df74fe0bdd434f45bd1093
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-07-20 18:52:53 -0500

    renames vars in inmates/scraper/__main__.py

--- inmates/scraper/__main__.py
+++ inmates/scraper/__main__.py
@@ -9,5 +9,6 @@ from sys import executable as python
 anchor_formatter = lambda anchor: anchor.rstrip('County').strip().lower().replace('. ', '-')
 all_links = dict(handle_csv('inmates.csv', ('IL County', anchor_formatter), ('Roster Link', None)))
 spider_urls = dict((path.stem, all_links[path.stem]) for path in get_modules_from('inmates.scraper.spiders'))
-for this_spider_name, this_spider_domain in spider_urls.items():
-    run_proc(split_args(f'{python} -m scrapy crawl -a domain={this_spider_domain} {this_spider_name}'))
+
+for spider, url in spider_urls.items():
+    run_proc(split_args(f'{python} -m scrapy crawl -a domain={url} {spider}'))
