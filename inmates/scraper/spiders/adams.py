1c265f36a785c143c75c7efa16ae81dfa2c3302a
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-08-02 00:14:19 -0500

    adds handle_pdf for extracting text form pdf

--- inmates/scraper/spiders/adams.py
+++ inmates/scraper/spiders/adams.py
@@ -1,12 +1,34 @@
-# -*- coding: utf-8 -*-
-import scrapy
-import pdfminer
+from io import BytesIO
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfpage import PDFPage
+from scrapy import Spider
+from subprocess import run as run_proc
 
 
-class AdamsSpider(scrapy.Spider):
+class AdamsRoster(Spider):
     name = 'adams'
-    allowed_domains = ['adams']
-    start_urls = ['https://www.co.adams.il.us/Home/ShowDocument?id=4547']
 
     def parse(self, response):
         pass
+
+
+def handle_pdf(buffer: BytesIO, codec: str = 'utf-8'):
+    rsrcmgr = PDFResourceManager()  # shared context for resources
+    sio = StringIO()
+    laparams = LAParams()  # "layout params" for formatting output
+    device = TextConverter(rsrcmgr, sio, codec=codec, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    for page in PDFPage.get_pages(buffer):
+        interpreter.process_page(page)
+
+    text = sio.getvalue()
+
+    device.close()
+    sio.close()
+
+    return text
