da7d0a5d6053fde91e19f7f068c14be3802a9e17
Emmanuel I. Obi <withtwoemms@gmail.com>
2020-10-04 01:37:43 -0500

    adds means of parsing booking info alongside demographic info

--- inmates/scraper/spiders/will.py
+++ inmates/scraper/spiders/will.py
@@ -1,6 +1,9 @@
 import ipdb
 
+from pprint import pprint
+from scrapy import Request
 from scrapy import Spider
+from urllib.parse import urlparse
 
 
 class WillSpider(Spider):
@@ -26,27 +29,66 @@ class WillSpider(Spider):
     def __init__(self, *args, **kwargs):
       self.domain = kwargs.get('domain')
       self.start_urls = [self.domain] if self.domain else kwargs.get('start_urls')
+      self.parsed_urls = [urlparse(url) for url in self.start_urls]
 
     def parse(self, response):
+        """
+        TODO (withtwoemms) -- handle pagination
+        """
         table_body = response.xpath('//*[@id="Inmate_Index"]/div[2]/div[2]/table/tbody')[0]
-        next_page = response.css('.Next::attr(href)').get()
-        # TODO: a good clean way to scrape these details
-        inmate_detail_link = response.css('td.Name > a').get()
-
-        # in xpath, double slash any child node that matches the locator ('tr' in this case)
         rows = table_body.xpath('tr')
+        parsed_url = self.parsed_urls[0]
         for row in rows:
-            yield {
-                'Name': row.css('.Name').get(),
-                'InCustody': row.css('.InCustody::text').get(),
-                'Race': row.css('.Race::text').get(),
-                'Gender': row.css('.Gender::text').get(),
-                'Height': row.css('.Height::text').get(),
-                'Weight': row.css('.Weight::text').get(),
-                'MultipleBookings': row.css('.MultipleBookings::text').get(),
-                'HousingFacility': row.css('.HousingFacility::text').get()
-            }
+            profile_url = self.extract_profile_url(row)
+            yield Request(url=profile_url, callback=self.parse_profile)
 
+    def parse_profile(self, response):
+        demography = response.xpath('//div[@id="DemographicInformation"]/ul/li')
+        demographic_info = self.parse_demographic_data(demography)
+        bookings = response.xpath('//div[@id="BookingHistory"]/div[@class="Booking"]')
+        booking_data = [self.parse_booking_data(booking) for booking in bookings]
+        demographic_info['Bookings'] = booking_data
+        yield demographic_info
 
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+    def parse_demographic_data(self, demography):
+        demographic_info = {}
+        for element in demography:
+            key = element.xpath('label/text()').get()
+            value = element.xpath('span/text()').get()
+            demographic_info.update({key: value})
+        return demographic_info
+
+    def parse_booking_data(self, booking):
+        """
+        TODO (withtwoemms) -- parse charges
+        """
+        booking_data = booking.xpath('./div[@class="BookingData"]')
+        booking_info = self.parse_booking_info(booking_data)
+        bond_info = self.parse_bond_info(booking_data)
+        booking_info['bonds'] = bond_info
+        return booking_info
+
+    def parse_bond_info(self, booking_data):
+        bond_info = []
+        keys = booking_data.xpath('./div[@class="BookingBonds"]//table/thead//th/text()').getall()
+        vals = booking_data.xpath('./div[@class="BookingBonds"]//table/tbody//td/text()').getall()
+        for row in booking_data.xpath('./div[@class="BookingBonds"]//table/tbody/tr'):
+            vals = row.xpath('./td/text()').getall()
+            bond_info.append(dict(zip(keys, vals)))
+        return bond_info
+
+    def parse_booking_info(self, booking_data):
+        booking_info = {}
+        for element in booking_data.xpath('./ul[@class="FieldList"]/li'):
+            key = element.xpath('label/text()').get()
+            value = element.xpath('span/text()').get()
+            booking_info.update({key: value})
+        return booking_info
+
+    def extract_profile_url(self, row):
+        parsed_url = self.parsed_urls[0]
+        url = f'{parsed_url.scheme}://{parsed_url.netloc}'
+        path = row.xpath('./td[@class="Name"]/a/@href').get()
+        profile_url = f'{url}{path}'
+        return profile_url
+
